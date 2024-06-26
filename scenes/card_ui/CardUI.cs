52e0b851916656e31bc324c5bb6e82434c69105a
Bartlomiej Fatyga <batek254@gmail.com>
2024-06-26 20:21:33 +0200

    update CardUI

--- scenes/card_ui/CardUI.cs
+++ scenes/card_ui/CardUI.cs
@@ -9,10 +9,33 @@ public partial class CardUI : Control
     public ColorRect color;
     public Label state;
     public Area2D dropPointDetector;
+    public CardStateMachine cardStateMachine;
 
     public override void _Ready()
     {
         color = GetNode<ColorRect>("ColorRect");
         state = GetNode<Label>("State");
+        cardStateMachine = GetNode<CardStateMachine>("CardStateMachine");
+        cardStateMachine.Init(this);
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        cardStateMachine.OnInput(@event);
+    }
+
+    public void _OnGuiInput(InputEvent @event)
+    {
+        cardStateMachine.OnGUIInput(@event);
+    }
+
+    public void _OnMouseEntered()
+    {
+        cardStateMachine.OnMouseEntered();
+    }
+
+    public void _OnMouseExited()
+    {
+        cardStateMachine.OnMouseExited();
     }
 }
