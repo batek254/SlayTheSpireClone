21486dc3dfc027e76c7f1b4a196175d2e7c39765
Bartlomiej Fatyga <batek254@gmail.com>
2024-06-26 20:00:50 +0200

    Update CardReleasedState

--- scenes/card_ui/card_states/CardReleasedState.cs
+++ scenes/card_ui/card_states/CardReleasedState.cs
@@ -3,5 +3,9 @@ using System;
 
 public partial class CardReleasedState : CardState
 {
-	
+	public override void Enter()
+	{
+		cardUI.color.Color = Colors.DarkViolet;
+		cardUI.state.Text = "Released";
+	}
 }
